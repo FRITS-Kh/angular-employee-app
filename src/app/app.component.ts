dd9fa57de68e4a6557237fe693a30fbbd8212f1c
Yurii Herton <gerton88@gmail.com>
2024-03-05 20:02:45 +0200

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-employee-platform';
+export class AppComponent implements OnInit {
+  title = 'course-app-1';
+
+  constructor(private angularFirestore: AngularFirestore) {}
+
+  ngOnInit() {
+    this.angularFirestore
+      .collection('test')
+      .snapshotChanges()
+      .subscribe((items) => {
+        console.log(items.map((x) => x.payload.doc.data()));
+      });
+  }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  name: 'development',
+  firebase: {
+    config: {
+      apiKey: 'AIzaSyC1ZpHHY6EdkOJkcVvl4_hHr0Q33yV_rvQ',
+      authDomain: 'angular-employee-platform.firebaseapp.com',
+      projectId: 'angular-employee-platform',
+      storageBucket: 'angular-employee-platform.appspot.com',
+      messagingSenderId: '210207100158',
+      appId: '1:210207100158:web:342f74754bab9d4945a9af',
+    },
+  },
+};
