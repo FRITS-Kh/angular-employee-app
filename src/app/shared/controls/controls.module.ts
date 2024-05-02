1ad3d824fed929c0e84563abcc8af590c254c307
Yurii Herton <gerton88@gmail.com>
2024-05-02 15:51:04 +0300

    fix dependencies

--- src/app/shared/controls/controls.module.ts
+++ src/app/shared/controls/controls.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 
-import {
-  InputComponent,
-  FormFieldComponent,
-  PasswordComponent,
-  SelectComponent,
-  CheckboxesComponent,
-  RadiosComponent,
-  DateComponent,
-  DateRangeComponent,
-  AutocompleteComponent,
-} from './';
+import { InputComponent } from './input/input.component';
+import { FormFieldComponent } from './form-field/form-field.component';
+import { PasswordComponent } from './password/password.component';
+import { SelectComponent } from './select/select.component';
+import { CheckboxesComponent } from './checkboxes/checkboxes.component';
+import { RadiosComponent } from './radios/radios.component';
+import { DateComponent } from './date/date.component';
+import { DateRangeComponent } from './date-range/date-range.component';
+import { AutocompleteComponent } from './autocomplete/autocomplete.component';
 
 @NgModule({
   imports: [
